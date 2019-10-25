6584550d0e693b7967d4dd69a395344690755b9f
CoffeeChemist <c.kantypowicz@gmail.com>
2019-10-25 14:43:44 +0100

    Inital Commit

---
+++ Pizza Party/Assets/Scripts/MenuMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using UnityEngine.SceneManagement;
+
+public class MenuMove : MonoBehaviour
+{
+    private readonly int ID = 0;
+    private Player p;
+
+    private bool hasPressedActionKey = false;
+
+    void Start()
+    {
+        p = ReInput.players.GetPlayer(ID);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Checks if key gets pressed
+        if (!hasPressedActionKey)
+        {
+            hasPressedActionKey = p.GetButtonDown("Action");
+        }
+
+        if (hasPressedActionKey)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
