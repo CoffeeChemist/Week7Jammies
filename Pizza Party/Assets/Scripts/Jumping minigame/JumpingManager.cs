0c21922014dd57127f858f2f8e2e8985a3838f35
HUBERT JANUSZEWSKI <1900537@uad.ac.uk>
2019-10-26 15:09:25 +0100

    Setup for jumping minigame

--- Pizza Party/Assets/Scripts/Jumping minigame/JumpingManager.cs
+++ Pizza Party/Assets/Scripts/Jumping minigame/JumpingManager.cs
@@ -7,8 +7,28 @@ using UnityEngine.SceneManagement;
 public class JumpingManager : MonoBehaviour
 {
     public GameObject[] p_topping = new GameObject[4];
+    public Log[] logs = new Log[4];
     public Player[] p_input = new Player[4];
     public Sprite[] sprites = new Sprite[8];
+
+    
+    private float speed;
+    private float timer;
+
+    public bool[] InAir = new bool[4];
+    public bool[] Alive = new bool[4];
+
+
+    public struct Log
+    {
+        public GameObject _object;
+
+        public int _player;
+        public int _speed;
+
+
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +36,10 @@ public class JumpingManager : MonoBehaviour
 
         for (int i = 0; i < 4; i++)
         {
-            p_topping[i] = new GameObject();
+            p_topping[i] = new GameObject("Topping");
             p_topping[i].AddComponent<SpriteRenderer>();
             p_topping[i].AddComponent<Rigidbody2D>();
+            
         }
 
 
@@ -27,6 +48,22 @@ public class JumpingManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Random.value >= 0.75f && timer >= 3f)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                logs[i]._object = new GameObject("Log");
+                logs[i]._object.AddComponent<SpriteRenderer>();
+                logs[i]._object.AddComponent<Rigidbody2D>();
+                logs[i]._player = i;
+            }
+
+            timer = 0f;
+        }
+
+
+        timer += Time.deltaTime;
+
+
     }
 }
