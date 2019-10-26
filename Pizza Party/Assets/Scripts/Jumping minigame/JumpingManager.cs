2ac910e52ea2c61af4aed8f9d4642a377c2326b2
HUBERT JANUSZEWSKI <1900537@uad.ac.uk>
2019-10-26 11:48:53 +0100

    Added Base for jumping game

---
+++ Pizza Party/Assets/Scripts/Jumping minigame/JumpingManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using UnityEngine.SceneManagement;
+
+public class JumpingManager : MonoBehaviour
+{
+    public GameObject[] p_topping = new GameObject[4];
+    public Player[] p_input = new Player[4];
+    public Sprite[] sprites = new Sprite[8];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int i = 0; i < 4; i++)
+        {
+            p_topping[i] = new GameObject();
+            p_topping[i].AddComponent<SpriteRenderer>();
+            p_topping[i].AddComponent<Rigidbody2D>();
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
