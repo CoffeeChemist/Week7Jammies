f36905e487ef51ebdf316468c42d71c7935bf9b2
Hubert Januszewski <hubithekazak@gmail.com>
2019-10-27 16:05:43 +0000

    Intro n stuff

---
+++ Pizza Party/Assets/Scripts/SkipIntro.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Rewired;
+
+public class SkipIntro : MonoBehaviour
+{
+    private Player p;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p = ReInput.players.GetPlayer(0);
+        timer = 15;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (p.GetButton("Action"))
+        {
+            SceneManager.LoadScene(5);
+
+        }
+
+        if (timer ==0)
+        {
+
+            SceneManager.LoadScene(5);
+        }
+        timer--;
+
+    }
+}
